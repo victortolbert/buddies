3a34e12cd03bd38664275aa771dee9847c9db293
Victor Tolbert <victor.tolbert@gmail.com>
2021-07-16 14:08:00 -0400

    fix: uncomment detail modal

--- vue/components/DetailButton.vue
+++ vue/components/DetailButton.vue
@@ -2,10 +2,10 @@
   <button @click="$store.commit('ui/OPEN_OVERLAY')">
     <slot>Details</slot>
     <Portal v-if="$store.state.ui.overlayIsOpen" to="overlays">
-      <!-- <Detail
+      <Detail
         :show="$store.state.ui.overlayIsOpen"
         @close="$store.commit('ui/CLOSE_OVERLAY')"
-      /> -->
+      />
     </Portal>
   </button>
 </template>
